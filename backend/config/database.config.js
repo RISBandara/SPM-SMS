950fbe3fea3f2a330fcd8b0bec336de5a4629fd4
Kasun2010 <kasunperera2010@gmail.com>
2018-09-08 17:59:50 +0530

    removed backend

--- backend/config/database.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * database configuration
- * @type {{secret: string, url: string}}
- */
-module.exports = {
-    secret: 'sms',
-    url: 'mongodb://localhost:27017/sms'
-}
--- frontend/src/common/header/header.jsx
+++ frontend/src/common/header/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './header.css';
 import Logo from "./../../IMG/logo.png";
+import ReactDOM from "react-dom";
+import Login from "../../views/login";
 
 export class Header extends React.Component {
     constructor(){
@@ -17,15 +19,30 @@ export class Header extends React.Component {
         console.log(this.state.username)
     }
 
+    logout(){
+        ReactDOM.render(<Login/>, document.getElementById('root'));
+    }
+
     render() {
         return (
             <div className="header">
                 <div className="row">
-                    <div className="image col-sm-9 col-md-9 col-lg-9 align-self-start">
+                    <div className="image col-sm-8 col-md-8 col-lg-8 align-self-start">
                         <img className="header-logo" src={Logo} alt=""/>
                     </div>
-                    <div className="col-sm-3 col-md-3 col-lg-3  align-self-start">
-                        <i className="far fa-user-circle"></i><label className="username">Welcome {this.state.username}</label>
+                    <div className="col-sm-3 col-md-2 col-lg-2  align-self-start">
+                        <div className="row">
+                            <div className="logout-logo col-sm-4"><i className="fas fa-user-circle"></i></div>
+                            <div className="logout-text col-sm-8"> <label className="username">Welcome {this.state.username}</label></div>
+                        </div>
+
+                    </div>
+                    <div className="col-sm-2 col-md-2 col-lg-2 align-self-start">
+                        <div className="row">
+                            <div className="logout-logo col-sm-7"><i className="fas fa-sign-out-alt"></i></div>
+                            <div className="logout-text col-sm-4"> <label onClick={this.logout} className="username">Logout</label></div>
+                        </div>
+
                     </div>
                 </div>
             </div>
