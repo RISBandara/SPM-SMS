3dfe30f3a44d18a3f532f808b46deacf2779e640
ISHAN <ishantha.sliit@gmail.com>
2018-09-08 21:57:39 +0530

    resolve merge conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Student_App from "./app_view/student_view";
+import Login from './views/login';
+import Company_App from './app_view/company_view';
 
 ReactDOM.render(<Login/>, document.getElementById('root'));
 
